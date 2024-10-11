e3868a8ded556ba86b3d595fc340d6c95fed47ba
Kanokpit Yowaratch <kanokpit_yow_fth@tdem.toyota-asia.com>
2024-10-12 03:21:43 +0700

    fix: update profile

--- app/profile/About.tsx
+++ app/profile/About.tsx
@@ -9,7 +9,6 @@ const About = () => {
         {`I'm a full stack web developer with 13+ years of experience in web development and coding. I love to create responsive websites using ReactJS, JavaScript, HTML5, CSS3, Bootstrap, Node js, expressjs and MongoDB.
         and  having good coding skills. I've done multiple small projects to learn basics and now doing more complex projects.`}
       </p>
-      <a href="./resume" target='_BLANK' className="btn btn-primary mt-2 custom-btn fw-bold">View My Resume</a>
       <h3 className='mt-10 pt-5 pb-2 custom-color'>Languages and tools I use:</h3>
 
       <p className='lead flex flex-wrap'>
@@ -17,17 +16,17 @@ const About = () => {
         <Image className='m-2' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40" />
         <Image className='m-2' src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="30" height="35" />
         <Image className='m-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react" width="35" height="35" />
-        <Image className='m-2' src="https://www.cdnlogo.com/logos/n/80/next-js.svg" alt="git" width="35" height="35" />
-        <Image className='m-2' src="https://www.cdnlogo.com/logos/a/51/angular.svg" alt="git" width="35" height="35" />
-        <Image className='m-2' src="https://cdn.cdnlogo.com/logos/n/49/node-js.svg" alt="git" width="50" height="50" />
-        <Image className='m-2' src="https://www.cdnlogo.com/logos/n/57/nestjs.svg" alt="git" width="40" height="40" />
-        <Image className='m-2' src="https://www.cdnlogo.com/logos/p/3/python.svg" alt="git" width="40" height="40" />
-        <Image className='m-2' src="/images/express-js.svg" alt="git" width="35" height="35" />
-        <Image className='m-2' src="https://cdn.cdnlogo.com/logos/m/25/mongodb.svg" alt="git" width="80" height="40" />
-        <Image className='m-2' src="https://www.cdnlogo.com/logos/p/25/prisma.svg" alt="git" width="80" height="40" />
+        <Image className='m-2' src="https://www.cdnlogo.com/logos/n/80/next-js.svg" alt="next-js" width="35" height="35" />
+        <Image className='m-2' src="https://www.cdnlogo.com/logos/a/51/angular.svg" alt="angular" width="35" height="35" />
+        <Image className='m-2' src="https://cdn.cdnlogo.com/logos/n/49/node-js.svg" alt="node-js" width="50" height="50" />
+        <Image className='m-2' src="https://www.cdnlogo.com/logos/n/57/nestjs.svg" alt="nestjs" width="40" height="40" />
+        <Image className='m-2' src="https://www.cdnlogo.com/logos/p/3/python.svg" alt="python" width="40" height="40" />
+        <Image className='m-2' src="/images/express-js.svg" alt="express-js" width="35" height="35" />
+        <Image className='m-2' src="https://cdn.cdnlogo.com/logos/m/25/mongodb.svg" alt="mongodb" width="80" height="40" />
+        <Image className='m-2' src="https://www.cdnlogo.com/logos/p/25/prisma.svg" alt="prisma" width="80" height="40" />
         <FaBootstrap className='mt-3 mb-2 ml-2 mr-2' style={{ width: "35", height: "35" }} />
         <Image className='m-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="git" width="35" height="35" />
-        <Image className='m-2' src="https://cdn.cdnlogo.com/logos/g/31/github.svg" alt="git" width="50" height="25" />
+        <Image className='m-2' src="https://cdn.cdnlogo.com/logos/g/31/github.svg" alt="github" width="50" height="25" />
       </p>
 
     </div>
--- app/resume/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CVPage() {
-  return (
-    <div className="full-page">
-      <iframe src='./files/full-stack-developer.pdf' />
-    </div>
-  );
-}
